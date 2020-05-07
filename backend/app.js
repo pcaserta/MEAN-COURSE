925538af0c125810a1e5bf170a8dcea93621ae2f
pcaserta <patrick.caserta@live.longwood.edu>
2020-05-07 10:12:16 -0400

    backend routes set up, but not functional

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First Middleware");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("second");
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "fad124122",
+      title: "First server-side post",
+      content: "This is coming from server",
+    },
+    {
+      id: "fawerwef",
+      title: "second server-side post",
+      content: "This is coming from server!!",
+    },
+  ];
+  res.status(200).json({ message: "Posts fetched succesfully", posts: posts });
 });
 
 module.exports = app;
