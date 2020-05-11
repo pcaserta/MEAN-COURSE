9e595c0c1c89210b935f0b829d4617608fe03887
pcaserta <patrick.caserta@live.longwood.edu>
2020-05-11 14:59:33 -0400

    cleaned up code

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -12,14 +12,14 @@ const MIME_TYPE_MAP = {
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("INVAli d mime type");
+    let error = new Error("Invaild mime type");
     if (isValid) {
       error = null;
     }
     cb(null, "backend/images");
   },
   filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split("").join("-");
+    const name = file.originalname.toLowerCase().split().join("-");
     const ext = MIME_TYPE_MAP[file.mimetype];
     cb(null, name + "-" + Date.now() + "." + ext);
   },
